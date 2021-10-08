7bc783531e67ca4c8366946c3ca5aad3e3f777a5
Vidhi Mittal <vmittal5@hawk.iit.edu>
2021-10-08 01:47:14 -0500

    testing

--- test_first_function.py
+++ test_first_function.py
@@ -1,37 +1,28 @@
 import pytest
 from main import Mashey_Assessment
-import sample_expected_asteroid_closest_approach
+import json
+from sample_expected_asteroid_closest_approach import Sample
 
 class TestClass:
 
-    def __init__(self):
-        
-        # To use the api_data.json file in all functions of the class
-        self.file1 = "asteroid_closest_approach.json"
-        self.file2 = "sample_expected_asteroid_closest_approach.json"
-        self.data1 = None
-        self.data2 = None
-        self.json_file1 = None
-        self.json_file2 = None
-
-
     def test(self):
 
-        with open(self.file1) as self.json_file1:
-            self.data1 = json.load(self.json_file1)
+        with open("asteroid_closest_approach.json") as json_file1:
+            data1 = json.load(json_file1)
 
-        with open(self.file2) as self.json_file2:
-            self.data2 = json.load(self.json_file2)
+        with open("sample_expected_asteroid_closest_approach.json") as json_file2:
+            data2 = json.load(json_file2)
 
-        temp1 = self.data1[0]
+        temp1 = data1[0]
 
-        temp2 = self.data2
-
+        print(temp1)
         
+        temp2 = data2
+
         assert temp1 == temp2
 
 
-        self.json_file1.close()
-        self.json_file2.close()
+        json_file1.close()
+        json_file2.close()
 
     
